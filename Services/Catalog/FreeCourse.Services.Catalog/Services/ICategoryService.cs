558f1e72b4265db0cb4dbfdf8e193d87b869bd11
umutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2022-11-27 06:22:43 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Shared.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    //Burası Neden internal yazınca hata vermiyor? public denedim hata verdi?
+    internal interface ICategoryService
+    {
+        Task<Response<List<CategoryDto>>> GetAllAsync();
+        Task<Response<CategoryDto>> CreateAsync(CategoryDto categoryDto);
+        Task<Response<CategoryDto>> GetByIdAsync(string id);
+    }
+}
